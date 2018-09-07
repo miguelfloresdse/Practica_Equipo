381a76a82b5f4e596b3194da4ecd625b9e046b90
Jose Luis <jose.luis.gonzalez.dse@uteq.edu.mx>
2018-09-07 12:35:17 -0500

    Agregue archivo jose luis

---
+++ joseluis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#define RESULTADO S
+
+int resta(int a, int b);
+float division(float a, float b);
+int suma(int a, int b);
+
+int main()
+{
+printf("¡Hola mundo!\n");
+division();
+return 0;
+}
